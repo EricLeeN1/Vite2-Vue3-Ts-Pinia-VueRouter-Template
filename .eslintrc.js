dcffb48c67cfb50bc644bd04e20f76325cff6bfa
EricLeeN1 <179310009@qq.com>
2022-01-15 22:40:30 +0800

    docs(auth): feac:增加请求等

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,78 +1,29 @@
-// @ts-check
-const { defineConfig } = require("eslint-define-config");
-module.exports = defineConfig({
-  root: true,
+module.exports = {
   env: {
     browser: true,
+    es2021: true,
     node: true,
-    es6: true,
   },
-  parser: "vue-eslint-parser",
+  extends: [
+    "plugin:vue/essential",
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    // eslint-config-prettier 的缩写
+    "plugin:prettier/recommended",
+    "prettier",
+    "plugin:jest/recommended",
+  ],
   parserOptions: {
+    ecmaVersion: 13,
     parser: "@typescript-eslint/parser",
-    ecmaVersion: 2020,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    // eslint-config-prettier 的缩写
-    "prettier",
-  ],
-  rules: {
-    "vue/script-setup-uses-vars": "error",
-    "@typescript-eslint/ban-ts-ignore": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "vue/custom-event-name-casing": "off",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "space-before-function-paren": "off",
-
-    "vue/attributes-order": "off",
-    "vue/one-component-per-file": "off",
-    "vue/html-closing-bracket-newline": "off",
-    "vue/max-attributes-per-line": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/attribute-hyphenation": "off",
-    "vue/require-default-prop": "off",
-    "vue/html-self-closing": [
-      "error",
-      {
-        html: {
-          void: "always",
-          normal: "never",
-          component: "always",
-        },
-        svg: "always",
-        math: "always",
-      },
-    ],
-  },
-});
+  // eslint-plugin-vue @typescript-eslint/eslint-plugin eslint-plugin-prettier的缩写
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: {},
+};
